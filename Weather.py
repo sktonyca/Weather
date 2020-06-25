d259d267f0f309f6b916568192b0acad2e5fa5ea
Tony <32371862+sktonyca@users.noreply.github.com>
2020-06-25 13:22:20 -0400

    Update Weather.py

--- Weather.py
+++ Weather.py
@@ -1,34 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Ask Location
-user_Input = input(
-    "Enter the name of the city\n")
-URL = "https://weather.gc.ca/city/jump_e.html?city="+user_Input+"&lang=e"
+while True:
+    # Ask Location
+    user_Input = input(
+        "Enter the name of the city\n")
+    URL = "https://weather.gc.ca/city/jump_e.html?city="+user_Input+"&lang=e"
 
-# Access Weather Data
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
+    # Access Weather Data
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-# If there is multiple option avaliable
-if soup.find("label", {"accesskey": "g"}).get_text() != None:
-    x = []
-    x = soup.find_all("ul")[3].findChildren()
-    del x[::2]
-    for i in range(len(x)):
-        print(i+1, x[i].get_text())
-    # Get input until valid option is given
-    while True:
-        try:
-            user_Input = x[int(
-                input("Select from the options above\n"))-1].get_text()
-            URL = "https://weather.gc.ca/city/jump_e.html?city="+user_Input+"&lang=e"
-            page = requests.get(URL)
-            soup = BeautifulSoup(page.content, 'html.parser')
-        except:
-            print("Input is invalid\nPlease Try Again")
-            continue
-        break
+    # If there is multiple option avaliable or city name is invalid
+    if soup.find("label", {"accesskey": "g"}).get_text() != None:
+        # If city name is invalid
+        if str(soup.find("main", class_="container")).find("No match found for") != -1:
+            print("Input is invalid")
+        else:
+            x = []
+            x = soup.find_all("ul")[3].findChildren()
+            del x[::2]
+            for i in range(len(x)):
+                print(i+1, x[i].get_text())
+            # Get input until valid option is given
+            while True:
+                try:
+                    user_Input = x[int(
+                        input("Select from the options above\n"))-1].get_text()
+                    URL = "https://weather.gc.ca/city/jump_e.html?city="+user_Input+"&lang=e"
+                    page = requests.get(URL)
+                    soup = BeautifulSoup(page.content, 'html.parser')
+                except:
+                    print("Input is invalid\nPlease Try Again")
+                    continue
+                break
+            break
+        continue
 
 # Get Location
 location = soup.find("h1", id="wb-cont").get_text()
